5e62aa0000ff118501d4dc9c7ce9c00b91573111
a22007560 <a22007560@alunos.ulht.pt>
2023-09-04 02:08:25 +0100

    New app structure

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,7 @@
+
 from flask_wtf import FlaskForm
-from flask_wtf.form import _Auto
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 from flask_babel import _, lazy_gettext as _l
 from app.models import User
 
@@ -29,32 +29,6 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError(_('Email already in use!'))
-        
-
-class EditProfileForm(FlaskForm):
-    username = StringField(_l('Username'), validators=[DataRequired()])
-    about_me = TextAreaField(_l('About me'), validators=[Length(min=0, max=140)])
-    submit = SubmitField(_l('Submit'))
-
-    def __init__(self, original_username, *args, **kwargs):
-        super(EditProfileForm, self).__init__(*args, **kwargs)
-        self.original_username = original_username
-
-    def validate_username(self, username):
-        if username.data != self.original_username:
-            user = User.query.filter_by(username=self.username.data).first()
-            if user is not None:
-                raise ValidationError(_('Username already in use.'))
-
-
-class EmptyForm(FlaskForm):
-    submit = SubmitField(_l('Submit'))
-
-
-class PostForm(FlaskForm):
-    post = TextAreaField(_l('Say something...'), validators=[DataRequired(), Length(min=1, max=140)])
-    submit = SubmitField(_l('Submit'))
-
 
 class ResetPasswordRequestForm(FlaskForm):
     email = StringField(_l('Email'), validators=[DataRequired(), Email()])
--- app/models.py
+++ app/models.py
@@ -3,8 +3,9 @@ from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from time import time
+from flask import current_app
 import jwt
-from app import db, login, app
+from app import db, login
 
 followers = db.Table(
     'followers',
@@ -63,12 +64,12 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256')
+            current_app.config['SECRET_KEY'], algorithm='HS256')
     
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+            id = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
         except:
             return
         return User.query.get(id)
@@ -83,6 +84,7 @@ class Post(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    language = db.Column(db.String(5))
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
